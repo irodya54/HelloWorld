ba90a7c1ae2ef5f2963112495e192bba55c4798f
Radion Merzkyakov <ya.irodya54@yandex.ru>
2022-07-06 18:49:53 +0300

    change util new

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,9 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.jdbc.Driver;
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.model.User;
-
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+
 
 public class Util {
 
