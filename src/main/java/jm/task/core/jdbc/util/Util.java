41c14a80ee7913dfa86756d5001de41d1414789e
Radion Merzkyakov <ya.irodya54@yandex.ru>
2022-07-10 15:37:40 +0300

    Connect Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,47 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.SessionFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory sessionFactory;
     public static Connection getConnection() throws SQLException {
 
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/newschema", "root", "Tuzikson2607DB");
 
+    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL,"jdbc:mysql://localhost:3306/newschema");
+                settings.put(Environment.USER,"root");
+                settings.put(Environment.PASS,"Tuzikson2607DB");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistry service = new StandardServiceRegistryBuilder().
+                        applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(service);
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
     }
 }
